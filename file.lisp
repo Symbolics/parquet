49d0ec76a2c31040028cdccfe0c291a69738e21a
Inchul <ijung@mapr.com>
2017-12-18 16:01:42 +0900

    add formatting

--- file.lisp
+++ file.lisp
@@ -52,16 +52,12 @@
     collect (read-byte s nil nil)))
 
 
-;;(with-open-file (s "./tests/tpch/customer.parquet" :element-type '(unsigned-byte 8))
-;;  (read-first-four-bytes s))
-
 (defun magic-number? (filename)
   "check first/last 'PAR1'"
   (with-open-file (s filename :element-type '(unsigned-byte 8))
     (and (equal '(80 65 82 49) (read-first-four-bytes s))
          (equal '(80 65 82 49) (read-last-four-bytes s)))))
 
-;; XXX : FIND OUT how to convert bytes-array into string (coerce '(80 65 82 49) 'string)
 
 (defun read-u32 (in)
   "read 32 bits integer with litten endian"
@@ -73,58 +69,12 @@
     (the int32 u4)))
 
 
-;; (read-i32 ...)
 (defun footer-length (filename)
   "length of footer. First, check a magic-number 'PAR1' and then get a footer length"
   (with-open-file (s filename :element-type '(unsigned-byte 8))
     (and (magic-number? filename)
        (file-position s (- (file-length s) 8))
        (read-u32 s))))
-
-;;(footer-length "./tests/tpch/customer.parquet")
-
-
-;; (defun mkstr (&rest args)
-;;   (with-output-to-string (s)
-;;     (dolist (a args)
-;;       (princ a s))))
-
-;; (defun u-to-s (number bit)
-;;   "Convert an unsigned number to a signed number with `bit` length."
-;;   (if (and (plusp number)
-;;            (< number (ash 1 bit)))
-;;       (if (plusp (logand number (ash 1 (1- bit))))
-;;           (- number (ash 1 bit))
-;;           number)
-;;       (error "Out of bounds error (Number is beyond ~a bit)" bit)))
-
-;; (defun s-to-u (number bit)
-;;   "Convert a signed number to an unsigned number with `bit` length."
-;;   (if (and (<= (- (ash 1 (1- bit))) number)
-;;            (< number (ash 1 (1- bit))))
-;;       (if (minusp number)
-;;           (+ number (ash 1 bit))
-;;           number)
-;;       (error "Out of bounds error (Number is beyond ~a bit)" bit)))
-
-;; (defun var-ints (bytes &optional (results 0) (depth 0))
-;;   "serializing integers. ref: https://developers.google.com/protocol-buffers/docs/encoding#varints
-;;    ex) 1010 1100 0000 0010 => #b0101100 #b0000010 => 000 0010 ++ 010 1100 => 256 + 32 + 8 + 4 = 300"
-;;   (if (not (equal #b10000000 (logand #b10000000 (first bytes))))
-;;       (logior results (ash (logand #b01111111 (first bytes)) (* 7 depth)))
-;;       (if (= depth 0)
-;;           (var-ints (cdr bytes) (logior (logand #b01111111 (first bytes)) results) (+ 1 depth))
-;;           (var-ints (cdr bytes) (logior (ash (logand #b01111111 (first bytes)) (* 7 depth)) results) (+ 1 depth)))))
-
-;; (defun var-ints (s &optional (results 0) (depth 0))
-;;   "serializing integers from stream. ref: https://developers.google.com/protocol-buffers/docs/encoding#varints
-;;    ex) 1010 1100 0000 0010 => #b0101100 #b0000010 => 000 0010 ++ 010 1100 => 256 + 32 + 8 + 4 = 300"
-;;   (let ((byte (read-byte s nil nil)))
-;;     (if (not (equal #b10000000 (logand #b10000000 byte)))
-;;         (logior results (ash (logand #b01111111 byte) (* 7 depth)))
-;;       (if (= depth 0)
-;;           (var-ints s (logior (logand #b01111111 byte) results) (+ 1 depth))
-;;           (var-ints s (logior (ash (logand #b01111111 byte) (* 7 depth)) results) (+ 1 depth))))))
 
 
 (defun extract-key-value (kv field s)
@@ -143,137 +93,6 @@
      (extract-key-value kv field s))
     (t (error "Sorry. Unsupported Field/Type of FileMetaData"))))
 
-;; (defun extract-column-metadata (metadata field s)
-;;   "parse COLUMN-METADATA from byte streams"
-;;   (get-id-type s field)
-;;   ;;(format t "field : ~A, type : ~A ~%" (field-id field) (field-type field))
-;;   (cond
-;;     ((= 0 (field-type field)) (format t "end of column-metadata : ~A ~%" (file-position s)))
-;;     ((= 1 (field-id field))
-;;      ;; required Type
-;;      (setf (column-metadata-type metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 2 (field-id field))
-;;      ;; list <Encoding> encoding
-;;      (let ((len (list-len s)))
-;;        ;;(format t "list length : ~A ~%" len)
-;;        (loop for i from 1 to len
-;;              do (setf (column-metadata-encodings metadata)
-;;                       (push (zigzag-to-int (var-ints s)) (column-metadata-encodings metadata)))))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 3 (field-id field))
-;;      (let ((len (list-len s)))
-;;        ;;(format t "list length : ~A ~%" len)
-;;        (loop for i from 1 to len
-;;              do (setf (column-metadata-path-in-schema metadata)
-;;                       (push (apply #'mkstr (char-list s)) (column-metadata-path-in-schema metadata)))))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 4 (field-id field))
-;;      ;; required CompressionCodec codec
-;;      (setf (column-metadata-codec metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 5 (field-id field))
-;;      ;; required i64 num_values
-;;      (setf (column-metadata-num-values metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 6 (field-id field))
-;;      ;; required i64 totoal_uncompressed_size
-;;      (setf (column-metadata-total-uncompressed-size metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 7 (field-id field))
-;;      ;; required i64 totoal_compressed_size
-;;      (setf (column-metadata-total-compressed-size metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 8 (field-id field))
-;;      ;; list<keyValue>
-;;      (let ((len (list-len s)))
-;;        ;;(format t "list length : ~A ~%" len)
-;;        (loop for i from 1 to len
-;;              do (let ((kv (make-key-value))
-;;                       (field-info (make-field)))
-;;                   (extract-key-value kv field-info s)
-;;                   (setf (column-metadata-key-value-metadata metadata)
-;;                         (push kv (column-metadata-key-value-metadata metadata))))))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 9 (field-id field))
-;;      ;; i64 data-page-offset
-;;      (setf (column-metadata-data-page-offset metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 10 (field-id field))
-;;      ;; i64 index-page-offset
-;;      (setf (column-metadata-index-page-offset metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 11 (field-id field))
-;;      ;; i64 dictionay-page-offset
-;;      (setf (column-metadata-dictionary-page-offset metadata) (zigzag-to-int (var-ints s)))
-;;      (extract-column-metadata metadata field s))
-;;     ((= 12 (field-id field))
-;;      ;; opetional Statistics
-;;      (error "Sorry. No Yet Implemented Statisics"))
-;;     ((= 11 (field-id field))
-;;      (error "Sorry. No Yet Implemented PageEncodingStats"))
-;;     (t (error "Sorry. Unsupported Field/Type of Column-Metadata"))))
-
-
-;; (defun extract-column-chunk (columnchunk field s)
-;;   "parse COLUMN_CHUNK from byte stream"
-;;   (get-id-type s field)
-;;   ;;(format t "field : ~A, type : ~A ~%" (field-id field) (field-type field))
-;;   (cond
-;;     ((= 0 (field-type field)) (format t "end of column-chunk : ~A ~%" (file-position s)))
-;;     ((= 1 (field-id field))
-;;      (error "Sorry, Not Yet Implemented field"))
-;;     ((= 2 (field-id field))
-;;      ;; required i64 file-offset
-;;      (setf (column-chunk-file-offset columnchunk) (zigzag-to-int (var-ints s)))
-;;      (extract-column-chunk columnchunk field s))
-;;     ((= 3 (field-id field))
-;;      ;; optional COLUMNMETADATA meta-data
-;;      (let ((c-metadata (make-column-metadata))
-;;            (field-info (make-field)))
-;;        (extract-column-metadata c-metadata field-info s)
-;;        (setf (column-chunk-meta-data columnchunk) c-metadata))
-;;      (extract-column-chunk columnchunk field s))
-;;     ((= 4 (field-id field))
-;;      (error "Sorry, Not Yet Implemented 4 th field"))
-;;     ((= 5 (field-id field))
-;;      (error "Sorry, Not Yet Implemented 5 th field"))
-;;     ((= 6 (field-id field))
-;;      (error "Sorry, Not Yet Implemented 6 th field"))
-;;     ((= 7 (field-id field))
-;;      (error "Sorry, Not Yet Implemented 7 th field"))
-;;     (t (error "Sorry. Unsupported Field/Type of COLUMNCHUNK"))))
-
-
-;; (defun extract-row-group (rowgroup field s)
-;;   "parse ROW-GROUP from byte streams"
-;;   (get-id-type s field)
-;;   ;;(format t "field : ~A, type : ~A ~%" (field-id field) (field-type field))
-;;   (cond
-;;     ((= 0 (field-type field)) (format t "end of row-group : ~A ~%" (file-position s)))
-;;     ((= 1 (field-id field))
-;;      ;; list <ColumnChunk> cloumns
-;;      (let ((len (list-len s)))
-;;        ;;(format t "list length : ~A ~%" len)
-;;        (loop for i from 1 to len
-;;              do (let ((columnchunk (make-column-chunk))
-;;                       (field-info (make-field)))
-;;                   ;; extract column-chunk
-;;                   (extract-column-chunk columnchunk field-info s)
-;;                   (setf (row-group-columns rowgroup) (push columnchunk (row-group-columns rowgroup))))))
-;;      (extract-row-group rowgroup field s))
-;;     ((= 2 (field-id field))
-;;      ;; required i64 total-byte-size
-;;      (setf (row-group-total-byte-size rowgroup) (zigzag-to-int (var-ints s)))
-;;      (extract-row-group rowgroup field s))
-;;     ((= 3 (field-id field))
-;;      ;; required i64 num-rows
-;;      (setf (row-group-num-rows rowgroup) (zigzag-to-int (var-ints s)))
-;;      (extract-row-group rowgroup field s))
-;;     ((= 4 (field-id field))
-;;      ;; list <SortingColumn> sorting-columns
-;;      (error "Sorry, Not Yet Implemented SoringColumn"))
-;;     (t (error "Sorry. Unsupported Field/Type of FileMetaData"))))
 
 (defun extract-schema-element (schema field s)
   "parse SCHEMA-ELEMENT from byte streams"
